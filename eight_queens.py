5b0f4794aed2414f67b388f8375ed2456b3457df
DC David <dcdavid@tutanota.com>
2020-12-24 11:54:31 -0500

    Update eight_queens.py

--- eight_queens.py
+++ eight_queens.py
@@ -5,16 +5,32 @@
 #    then it is in the "e file" (5th column) and "4th rank" (4th column)
 class Queen:
     def __init__(self, position):
-        self.file = ord(position[0]) - 96
+        self.file = position[0]
         self.rank = int(position[1])
+    
+    # --------------------------------------------------------
+    # Static methods to get the next/previous files and ranks
+    @staticmethod
+    def next_file(file):
+        return chr(ord(file) + 1)
+    @staticmethod
+    def prev_file(file):
+        return chr(ord(file) - 1)
+    @staticmethod
+    def next_rank(file):
+        return rank + 1
+    @staticmethod
+    def prev_rank(file):
+        return rank - 1
+    # --------------------------------------------------------
     
     # Check for intersections in the file "|"
     def check_file(self, queen2):
-        pass
+        return self.file == queen2.file
     
     # Check for intersections in the rank "—"
     def check_rank(self, queen2):
-        pass
+        return self.rank == queen2.rank
     
     # Check for intersections in the rising diagonal "⟋"
     def check_rising_diagonal(self, queen2):
@@ -26,12 +42,12 @@ class Queen:
     
     # Check for intersections in the file, rank, and both diagonals
     def intersects_with(self, queen2):
-        pass
+        Queen.next_file(self.file)
 
 q1 = Queen('a3')
 q2 = Queen('a3')
 
-print(q1.rank - 2)
+q1.intersects_with(q2)
 
 
 
