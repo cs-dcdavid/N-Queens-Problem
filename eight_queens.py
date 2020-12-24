dc3426b23523e8395c27c7d37e6276aba1ce3815
DC David <dcdavid@tutanota.com>
2020-12-24 13:07:38 -0500

    Implemented checks for files, ranks, rising/falling diagonals, and same positions.

--- eight_queens.py
+++ eight_queens.py
@@ -5,49 +5,113 @@
 #    then it is in the "e file" (5th column) and "4th rank" (4th column)
 class Queen:
     def __init__(self, position):
+        self.position = position
         self.file = position[0]
         self.rank = int(position[1])
+    
     
     # --------------------------------------------------------
     # Static methods to get the next/previous files and ranks
     @staticmethod
-    def next_file(file):
-        return chr(ord(file) + 1)
+    def next_files(file):
+        files = []
+        while(file != 'h'):
+            file = chr(ord(file) + 1)
+            files.append(file)
+        return files
+    
     @staticmethod
-    def prev_file(file):
-        return chr(ord(file) - 1)
+    def prev_files(file):
+        files = []
+        while(file != 'a'):
+            file = chr(ord(file) - 1)
+            files.append(file)
+        return files
+    
     @staticmethod
-    def next_rank(file):
-        return rank + 1
+    def next_ranks(rank):
+        ranks = []
+        while(rank != 8):
+            rank = rank + 1
+            ranks.append(rank)
+        return ranks
+    
     @staticmethod
-    def prev_rank(file):
-        return rank - 1
+    def prev_ranks(rank):
+        ranks = []
+        while(rank != 1):
+            rank = rank - 1
+            ranks.append(rank)
+        return ranks
     # --------------------------------------------------------
     
+    
+    # --------------------------------------------------------
+    # Class Methods to check intersections in the file, rank, and both diagonals
+    
+    # Check for two queens in the same position
+    @classmethod
+    def same_position(cls, queen1, queen2):
+        return queen1.position == queen2.position
+    
     # Check for intersections in the file "|"
-    def check_file(self, queen2):
-        return self.file == queen2.file
+    @classmethod
+    def same_file(cls, queen1, queen2):
+        return queen1.file == queen2.file
     
     # Check for intersections in the rank "—"
-    def check_rank(self, queen2):
-        return self.rank == queen2.rank
+    @classmethod
+    def same_rank(cls, queen1, queen2):
+        return queen1.rank == queen2.rank
     
     # Check for intersections in the rising diagonal "⟋"
-    def check_rising_diagonal(self, queen2):
-        pass
+    @classmethod
+    def same_rising_diagonal(cls, queen1, queen2):
+        next_files = Queen.next_files(queen1.file)
+        prev_files = Queen.prev_files(queen1.file)
+        next_ranks = Queen.next_ranks(queen1.rank)
+        prev_ranks = Queen.prev_ranks(queen1.rank)
+        
+        prev_squares = [
+            prev_files[i]+str(prev_ranks[i])
+            for i in range(min(len(prev_files), len(prev_ranks)))
+        ]
+        
+        next_squares = [
+            next_files[i]+str(next_ranks[i])
+            for i in range(min(len(next_files), len(next_ranks)))
+        ]
+        
+        rising_diagonal_squares = prev_squares + next_squares
+        return queen2.position in rising_diagonal_squares
     
     # Check for intersections in the falling diagonal "⟍"
-    def check_falling_diagonal(self, queen2):
-        pass
+    @classmethod
+    def same_falling_diagonal(cls, queen1, queen2):
+        next_files = Queen.next_files(queen1.file)
+        prev_files = Queen.prev_files(queen1.file)
+        next_ranks = Queen.next_ranks(queen1.rank)
+        prev_ranks = Queen.prev_ranks(queen1.rank)
+        
+        prev_squares = [
+            prev_files[i]+str(next_ranks[i])
+            for i in range(min(len(prev_files), len(next_ranks)))
+        ]
+        
+        next_squares = [
+            next_files[i]+str(prev_ranks[i])
+            for i in range(min(len(next_files), len(prev_ranks)))
+        ]
+        
+        rising_diagonal_squares = prev_squares + next_squares
+        return queen2.position in rising_diagonal_squares
+    # --------------------------------------------------------
     
+    # --------------------------------------------------------
     # Check for intersections in the file, rank, and both diagonals
     def intersects_with(self, queen2):
-        Queen.next_file(self.file)
+        return Queen.same_position(self, queen2) or Queen.same_rank(self, queen2) or Queen.same_file(self, queen2) or Queen.same_rising_diagonal(self, queen2) or Queen.same_falling_diagonal(self, queen2)
 
-q1 = Queen('a3')
-q2 = Queen('a3')
-
-q1.intersects_with(q2)
-
+print(q1.intersects_with(q2))
 
 
